947c6f7d7d4d40e0b105c438996e4268aa1af616
Tim Disney <tim.disney@gmail.com>
2017-07-22 21:51:40 -0700

    Update tests

--- test/test-in-macros.js
+++ test/test-in-macros.js
@@ -45,14 +45,14 @@ test('a with combinator should work inside a macro', t => {
 test('sequencing combinators should work inside a macro', t => {
   let output = compileAndEval(`
   'lang sweet.js';
-  import { identifier } from '../combinators' for syntax;
+  import * as C from '../combinators' for syntax;
   import Parser, { ImmutableContext } from '../parser' for syntax;
   import * as H from '../helpers.js' for syntax;
 
   syntax m = ctx => {
     let ictx = new ImmutableContext(ctx);
-    let ident = identifier.map(ident => H.unwrap(ident).value);
-    let result = Parser.sequence(ident, ident, ident).run(ictx);
+    let ident = C.identifier.map(ident => H.unwrap(ident).value);
+    let result = C.sequence(ident, ident, ident).run(ictx);
     let [a, b, c] = result.right[0];
     if (a === 'foo' && b === 'bar' && c === 'baz') {
       return #\`true\`;
@@ -60,6 +60,30 @@ test('sequencing combinators should work inside a macro', t => {
     return #\`false\`;
   };
   output = m foo bar baz
+  `);
+  t.is(output, true);
+});
+
+test('delimiter combinators should allow matching inside', t => {
+  let output = compileAndEval(`
+  'lang sweet.js';
+  import * as C from '../combinators' for syntax;
+  import Parser, { ImmutableContext } from '../parser' for syntax;
+  import * as H from '../helpers.js' for syntax;
+
+  syntax m = ctx => {
+    let ictx = new ImmutableContext(ctx);
+    let ident = C.identifier.map(ident => H.unwrap(ident).value);
+    let inner = C.bracesInner(C.sequence(ident, ident, ident));
+    let result = inner.run(ictx);
+    
+    let [a, b, c] = result.right[0];
+    if (a === 'foo' && b === 'bar' && c === 'baz') {
+      return #\`true\`;
+    }
+    return #\`false\`;
+  };
+  output = m { foo bar baz }
   `);
   t.is(output, true);
 });
